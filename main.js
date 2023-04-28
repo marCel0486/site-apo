769d7c7b6b6286d1517f3049c6d5a57ff13c3a3e
marcelo04 <marceloqueiroz7894@gmail.com>
2023-04-28 16:53:24 -0300

    site-apo 6.0

--- main.js
+++ main.js
@@ -68,3 +68,26 @@ linksInternos.forEach((link)=> {
 })
 }
 initeScrollSuave()
+
+
+//menu mobile
+
+const btnMobile = document.querySelector('.btn-mobile')
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault()
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+      } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+      }
+}
+
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
+   
